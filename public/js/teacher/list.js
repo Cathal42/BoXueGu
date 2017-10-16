9b847aa73add9d209a004c02aaef72414350ab07
yang zonghong <yzhxts@126.com>
2017-10-16 09:15:26 +0800

    实现查看讲师信息功能

--- public/js/teacher/list.js
+++ public/js/teacher/list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'], function ($,template) {
+define(['jquery', 'template', 'bootstrap'], function ($, template) {
   $.ajax({
     type: 'get',
     url: '/api/teacher',
@@ -8,5 +8,20 @@ define(['jquery','template'], function ($,template) {
         $('#teacher_list').html(template('tpl_teaherList', {items: data.result}));
       }
     }
+  });
+
+  $('#teacher_list').on('click', 'a.check-info', function () {
+    var id = $(this).parent().data('id');
+    console.log(id);
+    // alert(123);
+    $.ajax({
+      type: 'get',
+      url: '/api/teacher/view',
+      data: {tc_id: id},
+      success: function (data) {
+        $('#teacherInfo').html(template('tpl_teacherInfo', data.result));
+        $('#teacherModal').modal();
+      }
+    });
   });
 });
