ed72238689661f9623eb1059de7ada17cf9d3a2c
yang zonghong <yzhxts@126.com>
2017-10-16 10:15:11 +0800

    实现启用/注销讲师功能

--- public/js/teacher/list.js
+++ public/js/teacher/list.js
@@ -1,4 +1,5 @@
 define(['jquery', 'template', 'bootstrap'], function ($, template) {
+  /*渲染讲师列表*/
   $.ajax({
     type: 'get',
     url: '/api/teacher',
@@ -10,6 +11,7 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
     }
   });
 
+  /*查看讲师信息*/
   $('#teacher_list').on('click', 'a.check-info', function () {
     var id = $(this).parent().data('id');
     console.log(id);
@@ -19,8 +21,38 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
       url: '/api/teacher/view',
       data: {tc_id: id},
       success: function (data) {
-        $('#teacherInfo').html(template('tpl_teacherInfo', data.result));
-        $('#teacherModal').modal();
+        if (data.code === 200) {
+          $('#teacherInfo').html(template('tpl_teacherInfo', data.result));
+          $('#teacherModal').modal();
+        }
+      }
+    });
+  });
+
+  /*注销与启用*/
+  $('#teacher_list').on('click', 'a.btnHandle', function () {
+    // alert(456);
+    var id = $(this).parent().data('id');
+    var status = $(this).data('status');
+    var _this = $(this);
+    $.ajax({
+      type: 'post',
+      url: '/api/teacher/handle',
+      data: {
+        tc_id: id,
+        tc_status: status
+      },
+      success: function (data) {
+        if (data.code === 200) {
+
+          if(data.result.tc_status===0){
+            _this.text('注 销');
+          }
+          else {
+            _this.text('启 用');
+          }
+          _this.data('status',data.result.tc_status);
+        }
       }
     });
   });
