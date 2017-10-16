a56f5bf8aa903ddd31561a9df70fe8b5fc5b3842
yang zonghong <yzhxts@126.com>
2017-10-16 16:58:56 +0800

    讲师添加/编辑函数封装

--- public/js/teacher/manage.js
+++ public/js/teacher/manage.js
@@ -1,56 +1,57 @@
-define(['jquery', 'template','form'], function ($, template) {
+define(['jquery', 'template', 'form'], function ($, template) {
   var s_id = {};
   s_id.tc_id = location.search.slice(1).split('=')[1];
-  // console.log(location.search);
-  console.log(s_id.tc_id);
 
   /*页面渲染*/
-  if(s_id.tc_id){
+  if (s_id.tc_id) {
     $.ajax({
       type: 'get',
       url: '/api/teacher/edit',
       data: s_id,
       success: function (data) {
-        if(data.code===200){
-          data.result.title='讲师编辑';
-          data.result.saveBtnText='保存';
-          $('.body.teacher').html(template('tpl_teacherEdit',data.result));
+        if (data.code === 200) {
+          data.result.title = '讲师编辑';
+          data.result.saveBtnText = '保存';
+          $('.body.teacher').html(template('tpl_teacherEdit', data.result));
         }
       }
     });
 
     /*保存编辑*/
-    $('.body.teacher').on('click','.btnSave',function () {
-      $('form').ajaxSubmit({
-        type: 'post',
-        url: '/api/teacher/update',
-        success: function () {
-          alert('提交成功');
-          location.href='/teacher/list';
-        },
-      });
+    $('.body.teacher').on('click', '.btnSave', function () {
+      propose('/api/teacher/update');
       return false;
     });
   }
-  else{
-    $('.body.teacher').html(template('tpl_teacherEdit',{
-      title:'讲师添加',
-      saveBtnText:'添 加',
+  else {
+    $('.body.teacher').html(template('tpl_teacherEdit', {
+      title: '讲师添加',
+      saveBtnText: '添 加',
       tc_gender: 0  // 默认给一个1,让男被选中
     }));
 
-    $('.body.teacher').on('click','.btnSave',function () {
-      $('form').ajaxSubmit({
-        type: 'post',
-        url: '/api/teacher/add',
-        success: function () {
-          alert('添加成功');
-          location.href='/teacher/list';
-        },
-      });
+    $('.body.teacher').on('click', '.btnSave', function () {
+      propose('/api/teacher/add');
       return false;
     });
   }
 
+  function propose(url) {
+    $('form').ajaxSubmit({
+      type: 'post',
+      url: url,
+      success: function (data) {
+        if (data.code === 200) {
+          if (url.indexOf('updata') > 0) {
+            alert('编辑成功')
+          }
+          else {
+            alert('添加成功');
+          }
+          location.href = '/teacher/list';
+        }
+      }
+    });
+  }
 
 });
